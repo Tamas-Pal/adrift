25679a0da7f340dce6651621e7911ee5b4f01e8d
Tamas-Pal <tamas.pal.hu@gmail.com>
2024-03-06 22:24:45 +0100

    re-path

--- src/sketch.js
+++ src/sketch.js
@@ -34,7 +34,7 @@ function renderAdjectivesP5(p5) {
   let shorterAxisLength = Math.min(canvasWidth, canvasHeight);
 
   p5.preload = function () {
-    font = p5.loadFont('../public/font/IBMPlexMono-Regular.ttf');
+    font = p5.loadFont('/font/SpaceMono-Regular.ttf');
   };
 
   p5.setup = function () {
